016da4469e4b516ef490c8d961c8ef1e0c3b3dc2
Mostafa Aldilati <mustafa_kjm@hotmail.com>
2024-02-13 23:07:36 -0500

    API Update

--- frontend/src/fetch.js
+++ frontend/src/fetch.js
@@ -3,6 +3,6 @@
  * Returns data as JSON {id: number, name: string}
  */
 export const fetchData = async () => {
-    const response = await fetch("http://18.234.52.80:5000/data");
+    const response = await fetch("http://54.234.114.163:5000/data");
     return response.json();
 };
