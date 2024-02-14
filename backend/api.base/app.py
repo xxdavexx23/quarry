f7ecb252602aa8873d14eb6e81066251eb7b9653
Mostafa Aldilati <mustafa_kjm@hotmail.com>
2024-02-13 23:06:01 -0500

    Github Action trigger

--- backend/api.base/app.py
+++ backend/api.base/app.py
@@ -75,7 +75,7 @@ class heroes(db.Model):
 @app.route('/')
 def hello_world():
     """Returns a simple greeting."""
-    return 'Hello, Welcome to the DC Universe!'
+    return 'Hello, Welcome Aboard!'
 
 @app.route('/data')
 def get_data():
